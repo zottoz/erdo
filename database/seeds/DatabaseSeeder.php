9b8a84cfdc1ae14a3bf045d170382df3ceba3f32
zottoz <zottozbr@gmail.com>
2020-04-26 22:50:49 -0400

    Adc faker ppus

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Faker\Factory as Faker;
+use App\Ppu;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,5 +15,16 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+        $faker = Faker::create();
+    	foreach (range(1,100) as $index) {
+	        DB::table('ppus')->insert([
+	            'item'          => $faker->randomDigit,
+	            'descricao'     => $faker->realText($maxNbChars = 50, $indexSize = 2),
+                'um'            => $faker->randomElement($array = array ('m2','und','m', 'kg', 'sd')),	            
+                'quantidade'    => $faker->numberBetween($min = 1000, $max = 10000),
+                'valor'         => $faker->randomFloat($nbMaxDecimals = 2, $min = 10, $max = 100),
+                'contrato_id'   => 3,
+	        ]);
+	    }
     }
 }

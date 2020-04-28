2c3d4d6b342999c851ad7823d71c28f6c179a789
zottoz <zottozbr@gmail.com>
2020-04-28 13:54:04 -0400

    atual criar rdo - dados fake migration

--- database/migrations/2020_04_28_171140_test__p_p_u_s_population.php
+++ database/migrations/2020_04_28_171140_test__p_p_u_s_population.php
@@ -17,49 +17,49 @@ class TestPPUSPopulation extends Migration
         //cria empresa fake
         DB::statement("
         INSERT INTO empresas(id, razao, cnpj, contato, telefone) VALUES
-        (0, 'Empresa Enrrolada SA', '11.111.111/0001-00', 'Isaak Newton', '92-9999-9999')");
+        (10, 'Empresa Enrrolada SA', '11.111.111/0001-00', 'Isaak Newton', '92-9999-9999')");
 
         DB::statement("
         INSERT INTO empresas(id, razao, cnpj, contato, telefone) VALUES
-        (0, 'Trambulho Trabalhos Ltda', '22.222.2222/0001-00', 'Albert Einstein', '92-8888-9999')");
+        (20, 'Trambulho Trabalhos Ltda', '22.222.2222/0001-00', 'Albert Einstein', '92-8888-9999')");
 
         //cria contrato fake
         DB::statement("
         INSERT INTO contratos(id, numero, objeto, inicio, fim, empresa_id) VALUES
-        (0, '4600001111', 'servicos gerais', '2020-4-4', '2021-4-4', 1)");
+        (2, '4600001111', 'servicos gerais', '2020-4-4', '2021-4-4', 10)");
 
         DB::statement("
         INSERT INTO contratos(id, numero, objeto, inicio, fim, empresa_id) VALUES
-        (0, '4600001111', 'servicos gerais', '2020-4-4', '2021-4-4', 1)");
+        (3, '4600001111', 'servicos gerais', '2020-4-4', '2021-4-4', 20)");
 
         //cria os itens da ppu fake
         DB::statement("
         INSERT INTO ppus(id, item, descricao, um, quantidade, valor, contrato_id) VALUES
-        (0, 1, 'Limp e Conserv de Áreas Internas-COA', 'MES', 24, 2658, 1)");
+        (1, 1, 'Limp e Conserv de Áreas Internas-COA', 'MES', 24, 2658, 2)");
 
         DB::statement("
         INSERT INTO ppus(id, item, descricao, um, quantidade, valor, contrato_id) VALUES
-        (0, 2, 'Limp e Consv Áreas Livres e Externas-COA', 'MES', 24, 2100, 1)");
+        (2, 2, 'Limp e Consv Áreas Livres e Externas-COA', 'MES', 24, 2100, 2)");
 
         DB::statement("
         INSERT INTO ppus(id, item, descricao, um, quantidade, valor, contrato_id) VALUES
-        (0, 3, 'Jardinagem/Manut. Paisagística-COA', 'MES', 24, 2658, 1)");
+        (3, 3, 'Jardinagem/Manut. Paisagística-COA', 'MES', 24, 2658, 2)");
 
         DB::statement("
         INSERT INTO ppus(id, item, descricao, um, quantidade, valor, contrato_id) VALUES
-        (0, 6, 'Roçagem de Vegetação Alta-COA', 'M2', 600000, 0.37, 1)");
+        (4, 6, 'Roçagem de Vegetação Alta-COA', 'M2', 600000, 0.37, 2)");
 
         DB::statement("
         INSERT INTO ppus(id, item, descricao, um, quantidade, valor, contrato_id) VALUES
-        ( 0, 12, 'Serv de Desinset Desrat', 'MES', 24, 1000, 1)");
+        (5, 12, 'Serv de Desinset Desrat', 'MES', 24, 1000, 2)");
 
         DB::statement("
         INSERT INTO ppus(id, item, descricao, um, quantidade, valor, contrato_id) VALUES
-        (0, 15, 'Higieniz de Reserv c/Capac até 2000L-COA', 'UN', 36, 289.75, 1)");
+        (5, 15, 'Higieniz de Reserv c/Capac até 2000L-COA', 'UN', 36, 289.75, 2)");
 
         DB::statement("
         INSERT INTO ppus(id, item, descricao, um, quantidade, valor, contrato_id) VALUES
-        (0, 18, 'Serv manut predial (civil e hidro)-COA', 'MES', 24, 12448.56, 1)");
+        (5, 18, 'Serv manut predial (civil e hidro)-COA', 'MES', 24, 12448.56, 2)");
 
     }
 

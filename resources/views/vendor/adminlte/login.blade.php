99e5cf8ab2ea01f660ac71d6b7d296bf0869023c
zottoz <zottozbr@gmail.com>
2020-05-05 23:10:28 -0400

    alterado tela de login2

---
+++ resources/views/vendor/adminlte/login.blade.php
@@ -0,0 +1,98 @@
+@extends('adminlte::master')
+
+@section('adminlte_css_pre')
+    <link rel="stylesheet" href="{{ asset('vendor/icheck-bootstrap/icheck-bootstrap.min.css') }}">
+    <link rel="stylesheet" href="/custom/login.css">
+    <link rel="stylesheet" href="/custom/util.css">
+@stop
+
+@section('adminlte_css')
+    @stack('css')
+    @yield('css')
+@stop
+
+@section('classes_body', 'login-page')
+
+@php( $login_url = View::getSection('login_url') ?? config('adminlte.login_url', 'login') )
+@php( $register_url = View::getSection('register_url') ?? config('adminlte.register_url', 'register') )
+@php( $password_reset_url = View::getSection('password_reset_url') ?? config('adminlte.password_reset_url', 'password/reset') )
+@php( $dashboard_url = View::getSection('dashboard_url') ?? config('adminlte.dashboard_url', 'home') )
+
+@if (config('adminlte.use_route_url', false))
+    @php( $login_url = $login_url ? route($login_url) : '' )
+    @php( $register_url = $register_url ? route($register_url) : '' )
+    @php( $password_reset_url = $password_reset_url ? route($password_reset_url) : '' )
+    @php( $dashboard_url = $dashboard_url ? route($dashboard_url) : '' )
+@else
+    @php( $login_url = $login_url ? url($login_url) : '' )
+    @php( $register_url = $register_url ? url($register_url) : '' )
+    @php( $password_reset_url = $password_reset_url ? url($password_reset_url) : '' )
+    @php( $dashboard_url = $dashboard_url ? url($dashboard_url) : '' )
+@endif
+
+@section('body')
+	
+<div class="limiter">
+		<div class="container-login100">
+			<div class="wrap-login100">
+
+				<form class="login100-form validate-form" action="{{ $login_url }}" method="post">
+                
+                {{ csrf_field() }}
+
+					<span class="login100-form-title p-b-43">
+						e-RDO 
+					</span>
+					
+					
+					<div class="wrap-input100 validate-input" data-validate = "Digite um email válido: exemplo@erdo.com">
+						<input class="input100" type="text" name="email">
+						<span class="focus-input100"></span>
+						<span class="label-input100">Email</span>
+					</div>
+					
+					
+					<div class="wrap-input100 validate-input" data-validate="Informe a senha">
+						<input class="input100" type="password" name="password">
+						<span class="focus-input100"></span>
+						<span class="label-input100">Senha</span>
+					</div>
+
+					<div class="flex-sb-m w-full p-t-3 p-b-32">
+						<div class="contact100-form-checkbox">
+							<input class="input-checkbox100" id="ckb1" type="checkbox" name="remember-me">
+							<label class="label-checkbox100" for="ckb1">
+								Permanecer autenticado.
+							</label>
+						</div>
+
+						<div>
+							<a href="{{ route('login')}}" class="txt1">
+								Esqueceu a senha ?
+							</a>
+						</div>
+					</div>
+			
+
+					<div class="container-login100-form-btn">
+						<button type="submit" class="login100-form-btn">
+							Entrar
+						</button>
+					</div>
+					
+				</form>
+
+				<div class="login100-more" style="background-image: url('images/telalogin1.jpg');">
+				</div>
+			</div>
+		</div>
+	</div>
+	
+
+@stop
+
+@section('adminlte_js')
+    <script src="{{ asset('vendor/adminlte/dist/js/adminlte.min.js') }}"></script>
+    @stack('js')
+    @yield('js')
+@stop
